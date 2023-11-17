89cef80ec3715d5b0273a7cfcdbb17fb43a426cb
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-11-17 20:40:48 +0500

    Add project files.

---
+++ Tyuiu.PozhdinAA.Sprint5.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.PozhdinAA.Sprint5.Task0.V27.Lib;
+
+namespace Tyuiu.PozhdinAA.Sprint5.Task0.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\xMeT1oRx\source\repos\Tyuiu.PozhdinAA.Sprint5\Tyuiu.PozhdinAA.Sprint5.Task0.V27\bin\Debug\OutPutFileTask0.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
